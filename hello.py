04a8384bd630c173ce95502dccd52315efc7a006
albecabrera <albe.cabrera@yahoo.de>
2025-02-23 23:31:43 +0100

    First commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hola mundo")
+age = 20
+name = "Sergio"
+print(f"Mi nombre es {name} y tengo {age} años")
---
+++ variables.py
@@ -0,0 +1,10 @@
+'''Aprendiendo a aplicar las variables 
+    en Python
+    '''
+# A estudiar se ha dicho
+a = str(input("Cuál es tu nombre: "))
+b = str(input("Cuál es tu apellido: "))
+print("Mi nombre es", a)
+print("Mi apellido es", b)
+
+
