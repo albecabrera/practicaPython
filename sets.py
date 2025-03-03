37a2a28bad79a0037adaef1f20252894e2bb50c9
albecabrera <albe.cabrera@yahoo.de>
2025-03-03 10:22:33 +0100

    cambio4

---
+++ sets.py
@@ -0,0 +1,17 @@
+'''
+Listas desordenadas
+Datos inmutables
+Se pueden eliminar y añadir datos
+'''
+saludo = "hola"
+
+print(saludo[0])
+print(saludo[1])
+print(saludo[2])
+print(saludo[3])
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 # Para que se utilizan las tuplas 
+# Para datos constantes que no son modificables
 
 tupla = ("rojo", "azul", "verde")
 
 print(tupla[1]) #azul
 # Las tuplas son inmutables
+print(tupla.index("rojo"))
+
+
 
 
 
