259e173f99e3502bcad2f414a3212f86a254bc97
albecabrera <albe.cabrera@yahoo.de>
2025-03-03 19:38:12 +0100

    cambio8

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -15,3 +15,21 @@ print(comparacion)
 comparacion = a > b
 print(comparacion) # 10 no es mayor que 15, por lo tanto es falso
 
+# >=, <= mayor o menor igual que
+comparacion = a >= b
+print(comparacion)
+
+# and
+a = 15
+b = 17
+c = 13
+
+# comparaciones
+comparacion_1 = a < b and b > c # True
+comparacion_2 = a > b and b > c # False
+
+# comprobamos los resultados 
+print(comparacion_1)
+print(comparacion_2)
+
+
