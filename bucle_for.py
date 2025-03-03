3f33184ac33640e1886d52ac4206cbe573d1bd92
albecabrera <albe.cabrera@yahoo.de>
2025-03-03 23:20:39 +0100

    cambio12

---
+++ bucle_for.py
@@ -0,0 +1,10 @@
+# permite repetir bloques de código
+for i in range(5):
+    print(f'El valor del bucle es: {i}. ')	
+
+    print(range(5))
+
+print(list(range(5)))
+
+
+
---
+++ incremento.py
@@ -0,0 +1,21 @@
+# iteradores de incremento y de decremento
+
+a = 4
+# a = a + 3 Esta es la primera opción 
+a += 3 # Esta es la opción más aconsejable
+
+print(a)
+
+# Lo mismo sucede con la resta
+a = 4
+a = a - 3  #Esta es la primera opción 
+a -= 1 # Esta es la opción más aconsejable
+print(a)
+
+a = 10 
+a *= 5
+print(a)
+
+a = 10 
+a /= 5
+print(a)
