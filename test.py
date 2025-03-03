245aed282f06c439e3960d5f3e5631381c790b1b
albecabrera <albe.cabrera@yahoo.de>
2025-03-03 19:15:30 +0100

    cambio6

--- test.py
+++ test.py
@@ -7,8 +7,26 @@ print(tupla[1]) #azul
 # Las tuplas son inmutables
 print(tupla.index("rojo"))
 
+numero1 = 10
+numero2 = 20.5
+print(type(numero1 + numero2))
+print(type(numero1))
+print(type(numero2))
+print() # asi se deja un espacio entre líneas. 
+print(type(numero1 + int(numero2)))
 
 
+
+numero_1 = input("Introduzca el primer número: ") # Esto es un string
+numero_2 = input("Introduzca el segundo número: ") # Esto también es un string
+
+suma = numero_1 + numero_2
+print(f'El resultado de la suma es: {suma}.') # el resultado final es también un string
+
+numero_1 = int("10")
+numero_2 = int("30")
+suma = numero_1 + numero_2
+print(f"El resultado de la suma es: {suma} ")
 
 
 
